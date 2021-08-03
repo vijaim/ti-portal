6c074801859d5fdd988e884db93ca837d33f8bfd
Mukkandan-tgi <mukkandan.tgi@gmail.com>
2021-08-03 12:38:30 +0530

    removed logger and unwanted console logs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import logger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './redux-store/index'
 import Routes from './routes'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
+const logger = createLogger()
+let middleware = []
+if (process.env.NODE_ENV === 'production') {
+  middleware = [...middleware]
+} else {
+  middleware = [...middleware, logger]
+}
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)))
 
 ReactDOM.render(
   <Provider store={store}><Routes /></Provider>,
