28e661e551095bd4cde6662a04f6ca4b4102a4d1
Mukkandan-tgi <mukkandan.tgi@gmail.com>
2021-09-16 19:36:18 +0530

    tutorial sidebar alignment changes

--- src/component/insights/tutorial.jsx
+++ src/component/insights/tutorial.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import InsightsHeader from './insights-header'
 import { HEADING_TITLE } from '../../utils/constants'
 import TutorialContent from '../insights/tutorial-content'
+import './tutorial.css'
 
 const Tutorial = () => {
   const { TUTORIAL } = HEADING_TITLE
 
-  const sectionWidth = {
-    width: '1520px',
-    marginTop: '70px'
-  }
-
   return (
     <>
       <main>
-        <section className="bg-white pb-20 position-fixed shadow-sm" style={sectionWidth}>
+        <section className="bg-white pb-20 position-fixed shadow-sm section-width">
           <div className="container">
             <div className="align-items-center gy-2 mb-12 row">
               <div className="col-md col-sm">
