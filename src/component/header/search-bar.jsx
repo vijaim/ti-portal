83b3671ec31122864c7ed44981e4858b721935b5
Vijaianand Mohan <vijaianand@gmail.com>
2022-03-05 19:54:47 +0530

    Copy Text Change

--- src/component/header/search-bar.jsx
+++ src/component/header/search-bar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBar = ({ searchValue, onSearchValueChange }) => {
   return (
-    <input type="text" className="form-control" value = {searchValue} onChange={(e) => onSearchValueChange(e)} id="inputVerificationCode" placeholder="Search for a metric (e.g. page views, users) " required />
+    <input type="text" className="form-control" value = {searchValue} onChange={(e) => onSearchValueChange(e)} id="inputVerificationCode" placeholder="Search for a insight (e.g. page views, users) " required />
   )
 }
 
