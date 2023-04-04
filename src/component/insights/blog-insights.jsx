217cba9b254545e0a13f266027472ce56474a952
vijaysudarsanam98 <51353941+vijaysudarsanam98@users.noreply.github.com>
2023-04-04 16:56:12 +0530

    remove_managebutton

--- src/component/insights/blog-insights.jsx
+++ src/component/insights/blog-insights.jsx
@@ -1053,16 +1053,6 @@ const BlogInsights = (props) => {
               </nav>
             </div>
             <div className='d-flex align-items-center justify-content-between action-container'>
-              {/* {selectedTab.id !== 'all' && <span className="btn btn-primary disabled-link h5 pr-2 mx-auto" style={{ fontSize: 14 }} onClick={() => editCustomNarratives(selectedTab)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-pencil form-check-label" viewBox="0 0 16 16">
-                                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                            </svg>
-                          </span>} */}
-              {/* <span className="btn btn-primary disabled-link h4 addCustomBtn" onClick={() => setIsShowCustomInsightModal(true)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
-                  <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
-                </svg>
-              </span> */}
             </div>
           </div>
           <div className="container pb-40 pt-40">
